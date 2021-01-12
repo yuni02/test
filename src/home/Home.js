b98d9a2170342997345f3dd9509e3cdb04dc0135
hey880 <bibianna880@hanmail.net>
2021-01-13 04:35:26 +0900

    update Detail.js, Detail.css

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,6 @@
 import './Home.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Product from './Product';
 import product1 from '../img/product-1.jpg'
 import product2 from '../img/product-2.jpg'
@@ -14,8 +15,9 @@ function Home() {
             <img 
                 className="home__img"
                 src={board} alt=""/>
-
+            
                 <div className="home__row">
+                
                     <Product 
                     id="1"
                     title="양파즙"
@@ -24,6 +26,8 @@ function Home() {
                     price={6000}
                     hot={1}
                     />
+                
+               
                     <Product 
                     id="2"
                     title="양파즙"
@@ -32,6 +36,8 @@ function Home() {
                     price={6000}
                     rating={1}
                     />
+                
+              
                     <Product 
                     id="3"
                     title="양파즙"
@@ -40,6 +46,7 @@ function Home() {
                     price={6000}
                     rating={1}
                     />
+           
                     <Product 
                     id="4"
                     title="양파즙"
@@ -48,6 +55,7 @@ function Home() {
                     price={6000}
                     rating={1}
                     />
+           
                     <Product 
                     id="5"
                     title="양파즙"
@@ -56,6 +64,7 @@ function Home() {
                     price={6000}
                     rating={1}
                     />
+                
                 </div>
         </div>
     )
