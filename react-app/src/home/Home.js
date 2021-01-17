3c9159db24eead838034387eb24e3d1eb6e85371
Choe-song-i <hagwin@hanmail.net>
2021-01-17 16:19:28 +0900

    feat: add headerTrouble

---
+++ react-app/src/home/Home.js
@@ -0,0 +1,63 @@
+import './Home.css';
+import React from 'react';
+import Product from './Product';
+import product1 from '../img/product-1.jpg'
+import product2 from '../img/product-2.jpg'
+import product3 from '../img/product-3.jpg'
+
+
+
+
+function Home() {
+    return (
+        <div className="home">
+         
+
+
+                <div className="home__row">
+                    <Product 
+                    id="1"
+                    title="양파즙"
+                    image={product1}
+                    description="양파로 즙을 낸 것"
+                    price={6000}
+                    hot={1}
+                    />
+                    <Product 
+                    id="2"
+                    title="양파즙"
+                    image={product2}
+                    description="양파로 즙을 낸 것"
+                    price={6000}
+                    rating={1}
+                    />
+                    <Product 
+                    id="3"
+                    title="양파즙"
+                    image={product3}
+                    description="양파로 즙을 낸 것"
+                    price={6000}
+                    rating={1}
+                    />
+                    <Product 
+                    id="4"
+                    title="양파즙"
+                    image={product1}
+                    description="양파로 즙을 낸 것"
+                    price={6000}
+                    rating={1}
+                    />
+                    <Product 
+                    id="5"
+                    title="양파즙"
+                    image={product2}
+                    description="양파로 즙을 낸 것"
+                    price={6000}
+                    rating={1}
+                    />
+                </div>
+        </div>
+    )
+}
+
+export default Home;
