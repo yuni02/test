a77ac312ae10dfced53d8caa58123060641bf455
songi <hagwin@hanmail.net>
2021-01-23 00:33:00 +0900

    feat: add node_modules added:songi

--- src/detail/QnAForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {useStateValue} from '../StateProvider/StateProvider'
-
-function QnAForm(){
-    const [{user}, dispatch] = useStateValue(); 
-
-}
-
-export default QnAForm;
